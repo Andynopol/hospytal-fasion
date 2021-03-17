02d910b74820df3457016fcfa4266d92586d2d4a
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-03-18 01:02:36 +0200

    ok

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 import PageNotFound from './NotFountComponent';
 import Home from './Home';
 import Nav from './Navigation';
@@ -13,12 +14,18 @@ const Main: React.FC = () =>
     return (
         <Router>
             <Nav isVisible={ navVisibility } />
-            <Switch>
-                <Route exact path='/' component={ Home } />
-                <Route>
-                    <PageNotFound showNav={ setNavVisibility } />
-                </Route>
-            </Switch>
+            <Grid container>
+                <Grid item xs={ false } md={ 1 } lg={ 2 } />
+                <Grid item xs={ 12 } md={ 10 } lg={ 8 }>
+                    <Switch>
+                        <Route exact path='/' component={ Home } />
+                        <Route>
+                            <PageNotFound showNav={ setNavVisibility } />
+                        </Route>
+                    </Switch>
+                </Grid>
+                <Grid item xs={ false } md={ 1 } lg={ 2 } />
+            </Grid>
         </Router>
     );
 };
