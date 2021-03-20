b96d2c32322635be85e6bf4c77fe5d6ce37990fe
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-03-20 02:46:07 +0200

    Product icon shell

--- app/src/components/Home/index.tsx
+++ app/src/components/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './ProductCard';
 
 interface Props
 {
@@ -9,7 +10,7 @@ const Home = ( props: Props ) =>
 {
     return (
         <div>
-            Home
+            <Card name={ `test` } price={ 100 } currency={ [ 'lei' ] } details="asd" />
         </div>
     );
 };
