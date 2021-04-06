da55f473d30ce546aba9d71e58acfc5a3fa5e24b
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-04-06 22:14:40 +0300

    Update ProductCard.tsx

--- app/src/components/ProductCard.tsx
+++ app/src/components/ProductCard.tsx
@@ -137,7 +137,7 @@ const ProductCard = ( props: Props ) =>
 
 
             <CardMedia
-                className={ classes.media22 }
+                className={ classes.media }
                 children={ src ? null : <BrokenImageIcon className={ classes.icon } /> }
                 image={ src }
                 title="asd"
