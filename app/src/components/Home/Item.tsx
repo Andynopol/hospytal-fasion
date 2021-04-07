7e190064ea2c825424f1c459569faa703c10b73a
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-04-07 23:25:02 +0300

    ok

--- app/src/components/Home/Item.tsx
+++ app/src/components/Home/Item.tsx
@@ -17,6 +17,7 @@ interface Props
 
 const Item = ( props: Props ) =>
 {
+    //something changed
     const { _id, name, price, description, details, sale, stock, src, active } = props;
 
     return (
