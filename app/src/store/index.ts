e68f79528a27930984f3aa321daa8c8f473a5440
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-04-13 22:47:38 +0300

    Mobile error solved.
    Mobile devices crush if you are loading redux extensions. They do not have a integrated console.

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -4,4 +4,4 @@ import reducer from '../reducer';
 
 
 export const store = createStore( reducer, compose( applyMiddleware( thunk ), ( window as any ).__REDUX_DEVTOOLS_EXTENSION__ &&
-    ( window as any ).__REDUX_DEVTOOLS_EXTENSION__() ) );
+    ( window as any ).__REDUX_DEVTOOLS_EXTENSION__ ? ( window as any ).__REDUX_DEVTOOLS_EXTENSION__() : null ) );
