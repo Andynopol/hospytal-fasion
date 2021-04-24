1e71198bcb5e3e2023ae92a504b88420c384ef78
Andynopol <andrei.nechita95@gmail.com>
2021-04-24 22:31:46 +0300

    Comments

--- app/src/components/Main.tsx
+++ app/src/components/Main.tsx
@@ -22,72 +22,76 @@ import { snackbarActionManager, authentificationAction } from '../actions';
 
 
 //@main component that defines the routes
-const Main: React.FC = () =>
-{
+const Main: React.FC = () => {
+
+    /**
+     * TODO: Make a single From component
+     * TODO: Make the promotion and stock funtionality
+     * TODO: Make the cart
+     * TODO: !!!Make the payment functionality!!!
+     * TODO: Improve design
+     */
+
 
     //@snackbar controllers
     const dispatch = useDispatch();
 
     //@redux state for snackbar dinamic data
-    const snackInfo = useSelector( ( state: any ) => state.snackbar );
+    const snackInfo = useSelector((state: any) => state.snackbar);
 
     //@callback when snackbar closes
-    const handleSnackbarClose = ( event?: React.SyntheticEvent, reason?: string ) =>
-    {
-        if ( reason === 'clickaway' )
-        {
+    const handleSnackbarClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
             return;
         }
-        dispatch( snackbarActionManager.hide() );
+        dispatch(snackbarActionManager.hide());
     };
 
     /**visibility of navbar
      * ! false only on PageNotFound
     */
-    const [ navVisibility, setNavVisibility ] = useState( true );
+    const [navVisibility, setNavVisibility] = useState(true);
 
-    useEffect( () =>
-    {
-        if ( localStorage.getItem( 'profile' ) )
-        {
-            dispatch( authentificationAction.login( JSON.parse( localStorage.getItem( 'profile' ) ) ) );
+    useEffect(() => {
+        if (localStorage.getItem('profile')) {
+            dispatch(authentificationAction.login(JSON.parse(localStorage.getItem('profile'))));
         }
-    }, [] );
+    }, []);
 
     return (
         <Router>
-            <Nav isVisible={ navVisibility } />
+            <Nav isVisible={navVisibility} />
             <Grid container>
-                <Grid item xs={ 1 } md={ 1 } lg={ 2 } />
-                <Grid item xs={ 10 } lg={ 8 }>
+                <Grid item xs={1} md={1} lg={2} />
+                <Grid item xs={10} lg={8}>
                     <Switch>
-                        <Route exact path='/' component={ Home } />
-                        <Route exact path={ '/admin' } component={ Home } />
-                        <Route exact path='/admin/add-product' component={ AddProduct } />
-                        <Route path='/admin/product/:id' component={ UpdateProduct } />
-                        <Route path='/account' component={ AccountInfo } />
-                        <Route path='/login' component={ Login } />
-                        <Route path='/register' component={ Register } />
+                        <Route exact path='/' component={Home} />
+                        <Route exact path={'/admin'} component={Home} />
+                        <Route exact path='/admin/add-product' component={AddProduct} />
+                        <Route path='/admin/product/:id' component={UpdateProduct} />
+                        <Route path='/account' component={AccountInfo} />
+                        <Route path='/login' component={Login} />
+                        <Route path='/register' component={Register} />
                         <Route path='*'>
-                            <PageNotFound showNav={ setNavVisibility } />
+                            <PageNotFound showNav={setNavVisibility} />
                         </Route>
                     </Switch>
 
 
                     <Snackbar
-                        open={ snackInfo.open }
-                        autoHideDuration={ 4000 }
-                        onClose={ handleSnackbarClose }
-                        anchorOrigin={ {
+                        open={snackInfo.open}
+                        autoHideDuration={4000}
+                        onClose={handleSnackbarClose}
+                        anchorOrigin={{
                             vertical: 'bottom',
                             horizontal: 'right'
-                        } }>
-                        <Alert onClose={ handleSnackbarClose } severity={ snackInfo.variant }>
-                            { snackInfo.message }
+                        }}>
+                        <Alert onClose={handleSnackbarClose} severity={snackInfo.variant}>
+                            {snackInfo.message}
                         </Alert>
                     </Snackbar>
                 </Grid>
-                <Grid item xs={ 1 } lg={ 2 } />
+                <Grid item xs={1} lg={2} />
 
 
             </Grid>
