fa5557cefe5f95dbd1b41ffd0bcbd27f51b8ebf9
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-05-09 02:41:33 +0300

    ok

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,10 +27,10 @@ const ENTRYPOINT = `mongodb+srv://${ process.env.MONGO_USER }:${ process.env.MON
 app.use( express.static( path.join( __dirname, '../public' ) ) );
 
 
-// app.get( '/', function ( req, res )
-// {
-//     res.sendFile( path.join( __dirname, '../public', 'index.html' ) );
-// } );
+app.get( '/', function ( req, res )
+{
+    res.sendFile( path.join( __dirname, '../public', 'index.html' ) );
+} );
 
 
 
