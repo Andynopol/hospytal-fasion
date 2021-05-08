4b4ec9da2d2a5c15f92cdc9b9b56e4171d1b4fb8
Andynopol <41628030+Andynopol@users.noreply.github.com>
2021-05-09 01:58:45 +0300

    ok

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './components/Main';
-import './App.css';
 
-export default class App extends Component
+const App: React.FC = () =>
 {
-  render ()
-  {
-    return (
-      <Main />
-    );
-  }
-}
+  return (
+    <Main />
+  );
+};
+
+export default App;
+
